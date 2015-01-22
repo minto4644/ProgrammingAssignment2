ed38dc947416214a0ff519e817b43d5b37ca764b
minto1992 <myownway224533@gmail.com>
2015-01-22 23:13:27 +0530

    Update cachematrix.R
    added definition of function makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function that returns a list of functions for getting, setting values of matrix and their Inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 I <- NULL
