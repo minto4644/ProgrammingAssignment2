6d32facd79f07be715774d6de72a9187b91b08a9
minto1992 <myownway224533@gmail.com>
2015-10-10 11:17:32 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,40 +3,38 @@
 
 ## makeCacheMatrix is a function that returns a list of functions for getting, setting values of matrix and their Inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-I <- NULL
-set <- functions(z){
-        x <<- z
-        I <<- NULL
+makeCacheMatrix <- function(x){
+  I <- NULL
+  set <- function(y){
+    x <<- y
+    I <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(Inverse) I <<- Inverse
+ 
+  getInverse <- function() I
+  
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
-
-get <- function() x
-
-setInverse <- function(Inverse) I <<- Inverse
-
-getInverse <- function() I
-
-list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
-
-
-
-}
-
 
 ## This function calculates the inverse of matrix if not calulated beforhand.
 ## It first gets the inverse from makeCacheMatrix function. 
 ## If the given inverse is null then inverse is calulated using solve()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        I <- x$getInverse()
-        if(!is.null(I)){
-                message("returning chached data")
-                return(I)
-        }
-        
-        data <- x$get()
-        I <- solve(data, ...)
-        x$setInverse(I)
-        I
+cacheSolve <- function(x, ...){
+ 
+  I <- x$getInverse()
+ 
+  if(!is.null(I)){
+      message("getting chached data")
+      return(m)
+  }
+  
+ 
+  I <- solve(x$get, ...)
+  x$setInverse(I)
+  I
+  
 }
